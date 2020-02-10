bd188f400cd4a7a9aa2ee7f19dcb6caff6d9a7b2
Josh Blumberg <jlblumberg@gmail.com>
2020-02-10 15:18:32 +0000

    Add more complex test for '/bookmarks'

--- app.rb
+++ app.rb
@@ -1,7 +1,12 @@
 require 'sinatra/base'
 class BookmarkManager < Sinatra::Base
+
     get '/' do
         'Welcome to the BookMark Manager'
+    end
+
+    get '/bookmarks' do
+        'Here are your BookMarks'
     end
 
     run! if app_file ==$0
--- spec/features/bookmark_page_spec.rb
+++ spec/features/bookmark_page_spec.rb
@@ -1,6 +1,8 @@
 feature "visit BookMark page" do
-    scenario 'view Bookmar' do
+    scenario 'view Bookmarks' do
         visit '/bookmarks'
-        expect(page).to have_content "Here are your BookMarks"
+        expect(page).to have_content "https://www.facebook.com/"
+        expect(page).to have_content "https://www.google.com/"
+        expect(page).to have_content "https://www.youtube.com/"
     end
 end
